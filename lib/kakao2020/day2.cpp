410e2f60f0543a146a43a153679e25a799c0c1ec
WanseoKim <wseokim@gmail.com>
2020-12-16 00:01:51 +0900

    Create day2.cpp

---
+++ lib/kakao2020/day2.cpp
@@ -0,0 +1,10 @@
+// kakao2020-크레인 인형뽑기 게임
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    return answer;
+}
