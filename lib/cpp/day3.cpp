a84443be3a5b198b6416d7604e6a375cc00fc59f
WanseoKim <wseokim@gmail.com>
2020-12-18 08:12:23 +0900

    day3 solution

--- lib/cpp/day3.cpp
+++ lib/cpp/day3.cpp
@@ -1,24 +1,26 @@
 // 백준2839-설탕배달-greedy algorithm
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 
-int deno[] = {5, 3}; //큰 수부터 할당.
+int deno[] = {5, 3, 0}; //큰 수부터 할당. 여러개였다면 sort.
 
-// 약수의 최소 갯수 찾기
+// 뺄셈의 최소 횟수 찾기
 int main(){
     int cnt = 0;
     int N = 0;
     cin >> N;
     // subtract by largest num
     // until N is equal to or less than 0.
-    while(N>0){      
-        for(int i = 0; i < 2; i++){ 
-            N -= deno[i];
-            cnt++; // 봉지 갯수
+    //testcase:18->4,6->2
+    for(int i = 0; i < 2; i++){
+        while(N > deno[i+1]){ // N>=3
+            N -= deno[i]; 
+            cnt++; // 봉지 갯수 
+        } 
             
-        }
     }
-     
+         
     if(N != 0){
         cnt = -1;
     }
