300537a3476fcad1bf8a163bed8d3b0d6784d0e1
WanseoKim <wseokim@gmail.com>
2020-12-19 07:26:45 +0900

    sol

--- lib/cpp/sol7.cpp
+++ lib/cpp/sol7.cpp
@@ -4,19 +4,23 @@
 
 using namespace std;
 
-string solution(string number, int k) {
+string solution(string number, int k)
+{
     string s = "";
-    for (int i = 0; i<number.length(); i++){
-        while (!s.empty() && s.back() < number[i] && k>0){
+    for (int i = 0; i < number.length(); i++)
+    {
+        while (!s.empty() && s.back() < number[i] && k > 0)
+        {
             s.pop_back();
             k--;
         }
-        if(k==0){
+        if (k == 0)
+        {
             s += number.substr(i, number.length() - i);
             break;
         }
         s.push_back(number[i]);
     }
 
-    return s.substr(0, s.length()-k);
+    return s.substr(0, s.length() - k);
 }
