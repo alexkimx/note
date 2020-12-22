1873459a41d2ee04ca794e5d988725facba753a0
WanseoKim <wseokim@gmail.com>
2020-12-22 18:14:18 +0900

    Update sol14.cpp

--- lib/cpp/sol14.cpp
+++ lib/cpp/sol14.cpp
@@ -1,11 +1,22 @@
 // 백준-ATM
-#include <string>
+#include <iostream>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
 
 int main(){
-    
+    int n;
+    int p[1001];
+    int res = 0;
+    //input
+    cin >> n ;
+    for(int i=0; i<n; i++) cin >> p[i];
+    sort(p, p+n);
+    for(int i=0; i<n; i++){ // sum of elements in front
+        res += p[i]*(n-i);
+    }
+    //output
+    cout << res;
+    return 0;
 }
