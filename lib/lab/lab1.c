bfb52f8bb0845c7a54ed1905c2ff3da61f1c3698
Wanseo <wseokim@gmail.com>
2021-06-12 08:33:18 +0900

    lab1

--- lib/lab/lab1.c
+++ lib/lab/lab1.c
@@ -5,26 +5,9 @@ void gradeIf(int x){
     if(x>=90 && x<=100) printf("A");
     else if(x>=70 && x<90) printf("B");
     else if(x>=30 && x<70) printf("C");
-    else (x>=0 && x<30) printf("F");
+    else printf("F");
 
 }
-void gradeSwitch(int x){
-    switch (x)
-    {
-    case /* constant-expression */:
-        /* code */
-        break;
-    
-    default:
-        break;
-    }
-    if(x>=90 && x<=100) printf("A");
-    else if(x>=70 && x<90) printf("B");
-    else if(x>=30 && x<70) printf("C");
-    else (x>=0 && x<30) printf("F");
-
-}
-
 
 // method 2: switch
 int main(){
@@ -35,9 +18,6 @@ int main(){
     printf("Your grade:\n"); 
     printf("using if-statement:"); 
     gradeIf(num);
-    printf("\n")
-    printf("using switch-statement"); 
-    gradeSwitch(num); 
-    printf("\n")
+
     return 0; // end running
 }
