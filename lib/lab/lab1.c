7ddebcd4a60893748a695cb535282df619e731a3
Wanseo <wseokim@gmail.com>
2021-06-12 09:43:07 +0900

    lab2

--- lib/lab/lab1.c
+++ lib/lab/lab1.c
@@ -10,14 +10,42 @@ void gradeIf(int x){
 }
 
 // method 2: switch
+void gradeSwitch(int x){
+    // unfinished
+    
+    for (int i = 0; i < 4; i++)
+    {
+        switch (x)
+        {
+        case 90 ... 100:
+            printf("A");
+            break;
+        case 70 ... 89:
+            printf("B");
+            break;
+        case 30 ... 69:
+            printf("C");
+            break;
+        case 0 ... 29:
+            printf("F");
+            break;
+        default:
+            printf("TRY AGAIN\n");
+            break;
+        }
+    }
+}
+
+
+
 int main(){
     int num; // declare a variable
     
     num = scanf("%d",&num); // get input
 
     printf("Your grade:\n"); 
-    printf("using if-statement:"); 
     gradeIf(num);
+    //gradeSwitch(num);
 
     return 0; // end running
 }
--- lib/lab/lab2.c
+++ lib/lab/lab2.c
@@ -1,1 +1,20 @@
 // 홀수 짝수 프로그램
+#include <stdio.h>
+
+void check(int x){
+    if(x%2==1) printf("odd");
+    else printf("even");
+}
+int main(){
+    int N; // declare a variable
+    int K;
+    N = scanf("%d",&N); // get input
+    for (int i = 0; i < N; i++)
+    {
+        /* code */
+        scanf("%d",&K)
+        check(K);
+    }
+    
+    return 0; // end running
+}
