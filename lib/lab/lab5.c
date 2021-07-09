b80cfd93a15e352935c507c3575536622838f732
Wanseo <wseokim@gmail.com>
2021-07-09 19:54:37 +0900

    new folder python

--- lib/lab/lab5.c
+++ lib/lab/lab5.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 //포인터
-
-// define function 'give_three'
-void give_three(){
+// 목표:원하는 변수에 대해 '정의'된 '위치'에 3 담기
+// 원하는 위치에 3 담기
+// define 'give_three'
+void give_three(int &x){
+    &x = 3;
+    *x = 3;
+    x = 3;
 
 }
 
 int main()
 {
-	//
+	// 초기값
     int number = 0;
 	int number2 = 0;
 
-	// 
+	// 위치값을 정해준다. 
 	int choice = -1;
 
 	// 
---
+++ lib/lab/lab8.c
@@ -0,0 +1,26 @@
+//백준 더하기 사이클
+//https://www.acmicpc.net/problem/1110
+#include <stdio.h>
+int main(){
+    int num;
+    int a, b, c, d;
+    int cnt=0
+
+    scanf("%d", &num);
+    int init = num;
+    while (true)
+    {
+        /* code */
+        a = num % 10;
+        b = num / 10;
+        c = a+b;
+        d = 
+        cnt++;
+        if(a+b == init) break;
+    }
+    
+
+
+
+    return 0;
+}
